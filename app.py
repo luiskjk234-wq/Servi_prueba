0fd070645fae9f84e1faacaf3193deb0c3f15aa2
luiskjk234-wq <luiskjk234@gmail.com>
2025-12-25 13:26:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ DEMO_MODE = True
 db = mysql.connector.connect(
     host="localhost",
     user="axelbot_user",
-    password="LuisKjk345@#",
+    password="LUISkjk345@#",
     database="axelbot_demo"
 )
 
@@ -284,3 +284,4 @@ def registrar_log(numero, mensaje, respuesta):
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
+
